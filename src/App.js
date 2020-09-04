168a33253a425a55eed6ec57df5f05571b732741
Greis15 <62027284+Greis15@users.noreply.github.com>
2020-09-04 12:20:08 -0500

    agrgando imagen de banner

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import "./styles/styles.scss"
 import Curso from "./Curso"
+import banner from "./banner.jpg"
 
 const App = () => (
     //DEVUELVE UN ELEMENTO (DIV) dentro de este si puede haber mas hijos
@@ -24,7 +25,7 @@ const App = () => (
         <div className="main-banner img-container l-section" id="main-banner">
             <div className="ed-grid lg-grid-6">
                 <div className="lg-cols-4 lg-x-2">
-                <img className="main-banner__img" alt= "banner" src="https://images.pexels.com/photos/265614/pexels-photo-265614.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"/>
+                <img className="main-banner__img" alt= "banner" src= {banner}/>
                 <div className="main-banner__data s-center">
                     <p className="t2 s-mb-0">Curso de React</p>
                     <p> Subtítulo del banner</p>
